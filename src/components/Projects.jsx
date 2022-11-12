42144d000dee3a8e4af2f3648ee491e5838d88a7
jinyilu138 <jinyilu138@gmail.com>
2022-11-12 17:19:40 -0500

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	src/components/Projects.jsx
    #	src/components/Signin.jsx

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,8 +1,8 @@
-import { GridItem } from "@chakra-ui/react";
-import React from "react"
-import {useNavigate} from "react-router-dom"
+import { GridItem, Heading, Text } from '@chakra-ui/react';
+import React from "react";
+import {useNavigate} from "react-router-dom";
 import { Container } from "@chakra-ui/react";
-import { Box } from "framer-motion";
+import { Box, Button, ButtonGroup } from "@chakra-ui/react";
 
 import { Center } from "@chakra-ui/react";
 export default function Display (props){
@@ -12,22 +12,56 @@ export default function Display (props){
         
 
             <div>
-                
-                <Center margin= "80px" height= "300px" bg='tomato'>
+              <Center bg='white' margin= "80px" height= "300px" color='#CBD5E0'>
+                <Heading as='h2' size='2xl' colour='#CBD5E0' noOfLines={1}>
+                  Recommended Jobs:
+                </Heading>
+              </Center>
 
-                <Container centerContent={false} margin={1}>
-
-                    <h1 color="white">Web developer</h1>
-                            
-                     </Container>
-                    
-         
-
-
+                <Center margin= "80px" height= "300px" >
+                  <Box bg='#A0AEC0' height='300px' width='1400px' color='white'>
+                    <Heading as='h6' size='2xl'>
+                      Discription
+                    </Heading>
+                    <Box bg='#A0AEC0' height='60px' width='1400px' ></Box>
+                    <Center bg='#A0AEC0' h='100px' w='1100px' color='white'>
+                      <Text fontSize='lg'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</Text>
+                    </Center>
+                  </Box>
                 </Center>
-                
-                <button  onClick={()=> {navigate('/match')}}>Select a cause</button>
+              <Center margin= "10px" height= "5px" >
+                <button size='md' variant= 'outline' onClick={()=> {navigate('/match')}}>Select this cause</button>
+              </Center>
 
+              <Center margin= "80px" height= "300px" >
+                <Box bg='#A0AEC0' height='300px' width='1400px' color='white'>
+                  <Heading as='h6' size='2xl'>
+                    Discription
+                  </Heading>
+                  <Box bg='#A0AEC0' height='60px' width='1400px' ></Box>
+                  <Center bg='#A0AEC0' h='100px' w='1400px' color='white'>
+                    <Text fontSize='lg'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</Text>
+                  </Center>
+                </Box>
+              </Center>
+              <Center margin= "10px" height= "5px" >
+                <button size='md' variant= 'outline' onClick={()=> {navigate('/match')}}>Select this cause</button>
+              </Center>
+
+              <Center margin= "80px" height= "300px" >
+                <Box bg='#A0AEC0' height='300px' width='1400px' color='white'>
+                  <Heading as='h6' size='2xl'>
+                    Discription
+                  </Heading>
+                  <Box bg='#A0AEC0' height='60px' width='1400px' ></Box>
+                  <Center bg='#A0AEC0' h='100px' w='1400px' color='white'>
+                    <Text fontSize='lg'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</Text>
+                  </Center>
+                </Box>
+              </Center>
+              <Center margin= "10px" height= "5px" >
+                <button size='md' variant= 'outline' onClick={()=> {navigate('/match')}}>Select this cause</button>
+              </Center>
 
             </div>
            
