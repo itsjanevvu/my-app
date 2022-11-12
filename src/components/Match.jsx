44b3533699d86bf9a626488016ed50fc0a2ac923
jinyilu138 <jinyilu138@gmail.com>
2022-11-12 16:29:24 -0500

    match page updated

--- src/components/Match.jsx
+++ src/components/Match.jsx
@@ -7,56 +7,74 @@ export default function  Match (){
     return(
 
             <div>
+              <Box bg='white' height='60px' width='300px' color='#A0AEC0'></Box>
 
-              <Heading as='h2' size='3xl' noOfLines={1}>
-                Period.
-              </Heading>
-              <Heading as='h2' size='l' noOfLines={1}>
-                The menstrual movement.
-              </Heading>
+              <Center bg='white' h='100px' w='1650px' color='#CBD5E0'>
+                <Heading as='h2' size='2xl' colour='#CBD5E0' noOfLines={1}>
+                  Your Matched Job:
+                </Heading>
+              </Center>
 
-              <a href="https://period.org/"></a> {/*trying to hyperlink pictures but its not working :(*/}
-              <img src="https://cdn.rightgift.com/organizations/logos/2581d331-d666-4e00-9cd3-df2dcadad1cb/b94c4d9f-a745-41e4-b35b-17a2c4427b07/mobile.png"
+              <Center bg='white' h='400px' w='1650px' color='#CBD5E0'>
+                <a href="https://period.org/"></a> {/*trying to hyperlink pictures but its not working :(*/}
+                <img src="https://cdn.rightgift.com/organizations/logos/2581d331-d666-4e00-9cd3-df2dcadad1cb/b94c4d9f-a745-41e4-b35b-17a2c4427b07/mobile.png"
                    alt="nonprofit logo" width="500" height="500"/>
+              </Center>
 
-              <SimpleGrid columns={2} spacing={5}>
-                  <Box bg='#CBD5E0' height='100px' width='400px' color='white'>
-                    <Center bg='#CBD5E0' h='100px' w='400px' color='white'>
+
+              <SimpleGrid columns={3} spacing={5}>
+
+
+                <Box bg='#CBD5E0' height='100px' width='400px' color='white'>
+                  <Center bg='#CBD5E0' h='100px' color='white'>
+                    <Heading as='h6' size='lg'>
+                      Ideal Skills:
+                      <Heading as='h6' size='sm'>
+                        Python, Javascript, HTML
+                      </Heading>
+                    </Heading>
+                  </Center>
+                </Box>
+
+                <Box bg='#CBD5E0' height='100px' width='400px' color='white'>
+                    <Center bg='#CBD5E0' h='100px' color='white'>
                       <Heading as='h6' size='lg'>
-                      <Icon as={MdInsertEmoticon} />
-                         Cause
+                         Cause:
+                        <Heading as='h6' size='sm'>
+                          Menstruation Equity
+                        </Heading>
                       </Heading>
                     </Center>
-                    <Text fontSize='sm'>jhfgbjkghkf</Text>
                   </Box>
 
                 <Box bg='#CBD5E0' height='100px' width='400px' color='white'>
-                  <Center bg='#CBD5E0' h='100px' w='400px' color='white'>
+                  <Center bg='#CBD5E0' h='100px' color='white'>
                     <Heading as='h6' size='lg'>
-                      <Icon as={MdInsertEmoticon} />
-                      Number of Volunteers
+                      Number of Volunteers:
+                      <Heading as='h6' size='sm'>
+                        3
+                      </Heading>
                     </Heading>
                   </Center>
-                  <Text fontSize='sm'>jhfgbjkghkf</Text>
                 </Box>
               </SimpleGrid>
               <Box bg='white' height='60px' width='300px' color='#A0AEC0'></Box>
 
               <SimpleGrid columns={3} spacing={5}>
-                <Box bg='white' height='100px' width='200px' color='#A0AEC0'>
-                  <Center bg='white' h='100px' w='200px' color='#A0AEC0'>
+
+                <Center bg='white' h='100px' w='200px' color='#A0AEC0'>
                   <Heading as='h6' size='2xl'>
                     Discription
                   </Heading>
                   </Center>
-                </Box>
 
-              <Box bg='white' height='100px' width='30px' color='#A0AEC0'></Box>
+
+              <Box bg='white' height='100px' width='10px' color='#A0AEC0'></Box>
               <Center height='50px'><Divider orientation='vertical' /></Center>
+              <Box bg='white' height='100px' width='10px' color='#A0AEC0'></Box>
 
               <Box bg='white' height='100px' width='300px' color='#A0AEC0'>
                 <Center bg='white' h='100px' w='950px' color='#A0AEC0'>
-
                   <Text fontSize='lg'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</Text>
                   </Center>
                 </Box>
@@ -64,32 +82,12 @@ export default function  Match (){
 
               <Box bg='white' height='60px' width='300px' color='#A0AEC0'></Box>
 
-              <SimpleGrid columns={2} spacing={5}>
-                <Box bg='#CBD5E0' height='100px' width='400px' color='white'>
-                  <Center bg='#CBD5E0' h='100px' w='400px' color='white'>
-                    <Heading as='h6' size='lg'>
-                      <Icon as={MdInsertEmoticon} />
-                      Cause
-                    </Heading>
-                  </Center>
-                  <Text fontSize='sm'>jhfgbjkghkf</Text>
-                </Box>
+              <Center bg='white' h='400px' w='1650px' color='#CBD5E0'>
+                <Button colorScheme='#CBD5E0' variant='outline' size='lg'>
+                  Volunteer Now!
+                </Button>
+              </Center>
 
-                <Box bg='#CBD5E0' height='100px' width='400px' color='white'>
-                  <Center bg='#CBD5E0' h='100px' w='400px' color='white'>
-                    <Heading as='h6' size='lg'>
-                      <Icon as={MdInsertEmoticon} />
-                      Number of Volunteers
-                    </Heading>
-                  </Center>
-                  <Text fontSize='sm'>jhfgbjkghkf</Text>
-                </Box>
-              </SimpleGrid>
-
-
-              <Button colorScheme='#CBD5E0' size='lg' variant='link'>
-                Volunteer now
-              </Button>
             </div>
 
 
